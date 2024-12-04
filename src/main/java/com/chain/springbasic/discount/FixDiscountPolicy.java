6890946f41e78ebee9a6dee13252e362c086941e
이승환 <seaung13@gmail.com>
2024-12-05 06:16:16 +0900

    할인 정책 개발

---
+++ src/main/java/com/chain/springbasic/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.chain.springbasic.discount;
+
+import com.chain.springbasic.member.Grade;
+import com.chain.springbasic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price - discountFixAmount;
+        }
+        return 0;
+    }
+}
