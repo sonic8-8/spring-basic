ffde95278a596da3ef158d93967417f5ac9eadab
이승환 <154510650+sonic8-8@users.noreply.github.com>
2024-12-05 23:43:07 +0900

    feature/refactor-to-spring (#5)
    1. refactor: AppConfig를 기반으로 스프링 컨테이너 생성 후 스프링 빈 등록
    
    2. test: 빈 조회
    
    3. test: 동일한 타입이 둘 이상 있는 경우 빈 조회
    
    4. test: 부모 타입으로 조회 시 모든 자식 빈 조회
    
    5. test: XML 기반 스프링 컨테이너 생성 및 빈 조회, BeanDefinition 조회

--- src/main/java/com/chain/springbasic/AppConfig.java
+++ src/main/java/com/chain/springbasic/AppConfig.java
@@ -9,22 +9,29 @@ import com.chain.springbasic.member.MemberServiceImpl;
 import com.chain.springbasic.member.MemoryMemberRepository;
 import com.chain.springbasic.order.OrderService;
 import com.chain.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
---
+++ src/test/java/com/chain/springbasic/beanFind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,55 @@
+package com.chain.springbasic.beanFind;
+
+import com.chain.springbasic.AppConfig;
+import com.chain.springbasic.member.MemberService;
+import com.chain.springbasic.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @DisplayName("이름으로 스프링 빈 조회가 가능하다")
+    @Test
+    void findBeanByName() {
+        // given
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        // when then
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("이름 없이 타입으로 스프링 빈 조회가 가능하다")
+    @Test
+    void findBeanByType() {
+        // given
+        MemberService memberService = ac.getBean(MemberService.class);
+        // when then
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+
+    }
+
+    @DisplayName("구체 타입으로 스프링 빈 조회가 가능하다")
+    @Test
+    void findBeanByImplName() {
+        // given
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        // when then
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("등록되지 않은 빈은 예외가 발생한다")
+    @Test
+    void findBeanByNameX() {
+        // given when then
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberServiceImpl.class));
+    }
+
+}
